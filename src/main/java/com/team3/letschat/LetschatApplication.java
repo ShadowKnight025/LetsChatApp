0eba0f52784c24e4aa80c6ff2634a04c57b2de32
Trystan Meadows <trystan.meadows@genspark.net>
2022-05-23 12:54:48 -0500

    initial commit

---
+++ src/main/java/com/team3/letschat/LetschatApplication.java
@@ -0,0 +1,13 @@
+package com.team3.letschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetschatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/letschat/LetschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.letschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
